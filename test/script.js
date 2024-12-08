061b07a6fd16cba075d1380c13f01c0cd580fbdc
ai87645 <mesheryakovandreyy@yandex.ru>
2024-12-08 14:50:56 +0300

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,102 @@
+// console.log('тут что то будет')
+
+// let message = 'привет'
+
+// console.log(message)
+
+// message = 'пока'
+
+// console.log(message)
+
+
+
+
+// const message = 'привет'
+// const name = 'alex'
+// const admin = name
+// console.log(admin)
+
+
+
+// console.log(
+//     typeof 100
+// )
+
+
+// const num = 111
+// console.log(typeof num)
+// const str = '222'
+// console.log(typeof str)
+// result = num + str 
+// console.log(typeof result)
+// console.log(result)
+
+
+// alert('привет!')
+
+// confirm('ты готов?')
+
+// const age = +prompt('скольео тебе лет?')
+
+// switch (true) {
+//     case age < 1:{
+//         console.log('ага,ага.верю')
+//         break
+//     }
+//     case age < 13:{
+//         console.log('ну ты древний')
+//         break
+//     }
+//     case age === 14:{
+//         console.log('кайф,кент мой')
+//         break
+        
+//     }
+// }
+
+
+// let count = 0
+
+// while (count < 10) {
+//     console.log(count)
+//     count++
+// }
+
+
+
+
+// let count = 100
+
+// do {
+//     console.log(count)
+//     count++
+// } while (count < 10)
+
+
+// for (let i = -10; i < 6; i+=5) {
+//     alert(i)
+//     console.log('цикл кончил')
+// }
+
+
+
+// let count = 2
+
+// while (count < 15 ){
+//     count++
+//     console.log(count)
+// }
+
+
+
+// if (count%3===0){
+//     console.log('цикл досрочно закончен')
+// }
+
+
+let count = 6
+
+while (count<15){
+    console.log(count)
+    alert(count)
+}
